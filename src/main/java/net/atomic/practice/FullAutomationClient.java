7cacfe3554ce980a180b3cd07f289e0fc6194b9a
Connor Mulligan <cgmulligan33@gmail.com>
2024-01-28 16:40:31 -0600

    asdd

---
+++ src/main/java/net/atomic/practice/FullAutomationClient.java
@@ -0,0 +1,10 @@
+package net.atomic.practice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FullAutomationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/atomic/practice/FullAutomationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.atomic.practice;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FullAutomationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
