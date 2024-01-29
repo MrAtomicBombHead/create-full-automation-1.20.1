c25ec94051779d31c282b8f1de33e4f60ed7bb1e
Connor Mulligan <cgmulligan33@gmail.com>
2024-01-28 19:19:16 -0600

    it works. it depends on create.

--- src/main/java/net/atomic/fullautomation/FullAutomation.java
+++ src/main/java/net/atomic/fullautomation/FullAutomation.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FullAutomation implements ModInitializer {
-	public static final String MOD_ID = "practice";
+	public static final String MOD_ID = "fullautomation";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
