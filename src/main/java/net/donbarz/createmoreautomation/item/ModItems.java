350fffee5b28fe80825d8a324d501424ff576203
DonBArz <PaulAurelius@web.de>
2024-02-10 12:43:18 +0100

    Adding void goo

--- src/main/java/net/donbarz/createmoreautomation/item/ModItems.java
+++ src/main/java/net/donbarz/createmoreautomation/item/ModItems.java
@@ -14,9 +14,11 @@ public class ModItems {
 
     //public static final Item exampleItem = registerItem("example", new Item(new FabricItemSettings()));
     public static final Item COSMICSOOT = registerItem("cosmicsoot", new Item(new FabricItemSettings()));
+    public static final Item VOIDGOO = registerItem("voidgoo", new Item(new FabricItemSettings()));
     private static void addItemstoIngredientsCreativeTab(FabricItemGroupEntries entries){
         //entries.add(exampleItem);
         entries.add(COSMICSOOT);
+        entries.add(VOIDGOO);
     }
 
     public static Item registerItem(String name, Item item){
