a8440f385ef89fe136ffde3f6ed71e7104779df6
Connor Mulligan <cgmulligan33@gmail.com>
2024-02-23 19:37:54 -0600

    fixed inconsistencies

--- src/main/java/net/donbarz/createmoreautomation/item/ModItems.java
+++ src/main/java/net/donbarz/createmoreautomation/item/ModItems.java
@@ -1,8 +1,6 @@
 package net.donbarz.createmoreautomation.item;
 
 import net.donbarz.createmoreautomation.CreateMoreAutomation;
-import net.donbarz.createmoreautomation.entity.custom.InkBottleEntity;
-import net.donbarz.createmoreautomation.item.custom.InkBottleItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -17,13 +15,11 @@ public class ModItems {
     //public static final Item exampleItem = registerItem("example", new Item(new FabricItemSettings()));
     public static final Item COSMICSOOT = registerItem("cosmic_soot", new Item(new FabricItemSettings()));
     public static final Item VOIDGOO = registerItem("void_goo", new Item(new FabricItemSettings()));
-    public static final Item INKBOTTLE = registerItem("ink_bottle", new InkBottleItem(new FabricItemSettings()));
     public static final Item GLOWINKBOTTLE = registerItem("glow_ink_bottle", new Item(new FabricItemSettings()));
     private static void addItemstoIngredientsCreativeTab(FabricItemGroupEntries entries){
         //entries.add(exampleItem);
         entries.add(COSMICSOOT);
         entries.add(VOIDGOO);
-        entries.add(ModItems.INKBOTTLE);
         entries.add(GLOWINKBOTTLE);
     }
 
