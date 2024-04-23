5947f3202f6ec0ea605969fdec31e1b70f2a95b2
Connor Mulligan <cgmulligan33@gmail.com>
2024-04-23 06:33:19 -0500

    back to before

--- src/main/java/net/donbarz/createmoreautomation/CreateMoreAutomationClient.java
+++ src/main/java/net/donbarz/createmoreautomation/CreateMoreAutomationClient.java
@@ -13,6 +13,7 @@ public class CreateMoreAutomationClient implements ClientModInitializer {
         EntityRendererRegistry.register(InkBottleEntityType.INK_BOTTLE_ENTITY_TYPE, (context) ->
                 new FlyingItemEntityRenderer(context));
 
-        EntityRendererRegistry.register(GlowInkBottleEntityType.GLOW_INK_BOTTLE_ENTITY_TYPE, FlyingItemEntityRenderer::new);
+        EntityRendererRegistry.register(GlowInkBottleEntityType.GLOW_INK_BOTTLE_ENTITY_TYPE, (context) ->
+                new FlyingItemEntityRenderer(context));
     }
 }
